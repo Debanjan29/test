8e90865fa14988430b8270c4fd2fbf5e26555ab7
Debanjan29 <Debanjanbhatt29@gmail.com>
2025-03-08 22:38:17 +0530

    updated requirement

--- project/settings.py
+++ project/settings.py
@@ -87,19 +87,16 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-        #"ENGINE": "django.db.backends.sqlite3",
-        #"NAME": BASE_DIR / "db.sqlite3",
-    #}
-#}
-
+#Delete before pushing in production
 
 DATABASES = {
         'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL')
         
     )}
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
